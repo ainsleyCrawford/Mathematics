8926b429a97ca3d8fc0dd4ef541979899617927c
Ainsley Crawford <acrawford@spartaglobal.com>
2019-09-13 18:16:00 +0100

    Fibonacci numbers

---
+++ Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Mathematics
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //Printing Fibonacci numbers less than 500
+            Fibonacci_Numbers.Fibonacci();
+            Console.Read();
+        }
+    }
+    internal class Fibonacci_Numbers
+    {
+        public static void Fibonacci()
+        {
+            int a = 0; int b = 1; int c = 0;
+            while (c < 500)
+            {
+                Console.WriteLine(c);
+                c = a + b;
+                a = b;
+                b = c;
+            }
+        }
+    }
+}
