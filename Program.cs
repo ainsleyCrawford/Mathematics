0d64d0315151c006f27be019727c22c17eb7dbcd
Ainsley Crawford <acrawford@spartaglobal.com>
2019-09-13 19:23:14 +0100

    Fibonacci numbers & Prime Numbers

--- Program.cs
+++ Program.cs
@@ -12,6 +12,8 @@ namespace Mathematics
         {
             //Printing Fibonacci numbers less than 500
             Fibonacci_Numbers.Fibonacci();
+            //Prime numbers under 200
+            Prime_Numbers.Prime(); 
             Console.Read();
         }
     }
@@ -19,6 +21,7 @@ namespace Mathematics
     {
         public static void Fibonacci()
         {
+            Console.WriteLine("The Fibonacci numbers below 500 are the following:");
             int a = 0; int b = 1; int c = 0;
             while (c < 500)
             {
@@ -26,6 +29,36 @@ namespace Mathematics
                 c = a + b;
                 a = b;
                 b = c;
+            }
+        }
+    }
+    internal class Prime_Numbers
+    {
+        public static void Prime()
+        {
+            Console.WriteLine("The exhaustive list of less-than-two-digit prime numbers is:");
+            List<int> compositeNumbers = new List<int>();
+            for (int i = 1; i < 100; i++)
+            {
+                for (int divisor = 2; divisor <= (i/2); divisor++)
+                {
+                    if (i % divisor == 0)
+                    {
+                        compositeNumbers.Add(i);
+                    }
+                }
+            }
+            int numbersTo100 = 1;
+            while (numbersTo100 < 100)
+            {
+                if (compositeNumbers.Contains(numbersTo100))
+                {
+                }
+                else
+                {
+                    Console.WriteLine(numbersTo100);
+                }
+                numbersTo100++;
             }
         }
     }
