6af87081e77cca5dca15d42baea0f3efbd41ca3a
Ainsley Crawford <ainsley.c@hotmail.co.uk>
2023-09-16 11:49:59 +0100

    Refactored with the ternary operator, string interpolation and expression-bodied syntax

--- Program.cs
+++ Program.cs
@@ -57,27 +57,19 @@ namespace Mathematics
             if (Int32.TryParse(Console.ReadLine(), out d))
             {
                 Console.WriteLine("The Fibonacci numbers below "+ d +" are the following:");
-                int a = 0; int b = 1; int c = 0;
+                int a = 0, b = 1, c = 0;
                 while (c < d)
                 {
                     Console.Write(c);
                     c = a + b;
-                    if (c < d)
-                    {
-                        Console.Write(", ");
-                    } 
-                    else
-                    {
-                        Console.Write(".");
-                    }
+                    string punctuationMark = (c < d) ? ", " : ".";
+                    Console.Write(punctuationMark);
                     a = b;
                     b = c;
                 }
             }
-            else
-            {
-                Console.WriteLine("Invalid input");
-            }
+            else Console.WriteLine("Invalid input");
+
             Console.WriteLine("\n\nKey 1 to continue with this function, 2 to return to the main menu, or any other key to close the program.");
             switch (Console.ReadKey().Key)
             {
@@ -108,35 +100,30 @@ namespace Mathematics
                 {
                     for (int divisor = 2; divisor <= Math.Sqrt(i); divisor++)
                     {
-                        if (i % divisor == 0)
-                        {
-                            compositeNumbers.Add(i);
-                        }
+                        if (i % divisor == 0) compositeNumbers.Add(i);
                     }
                 }
                 int numbersTolimit = 1;
                 while (numbersTolimit < limit)
                 {
-                    if (compositeNumbers.Contains(numbersTolimit))
-                    {
-                    }
-                    else
-                    {
-                        Console.Write(numbersTolimit + ", ");
-                    }
+                    if (!compositeNumbers.Contains(numbersTolimit)) Console.Write(numbersTolimit + ", ");
+                    //{
+                    //}
+                    //else
+                    //{
+                    //    Console.Write(numbersTolimit + ", ");
+                    //}
                     numbersTolimit++;
                 }
             } 
-            else
-            {
-                Console.WriteLine("Invalid input");
-            }
+            else Console.WriteLine("Invalid input");
+            
             Console.WriteLine("\n\nKey 1 to continue with this function, 2 to return to the main menu, or any other key to close the program.");
             switch (Console.ReadKey().Key)
             {
                 case ConsoleKey.D1:
                     Console.Clear();
-                    Prime(); ;
+                    Prime();
                     break;
                 case ConsoleKey.D2:
                     Console.Clear();
@@ -155,18 +142,16 @@ namespace Mathematics
             Console.Write("Enter a value for n to find the corresponding nth triangular number: ");
             if (Int32.TryParse(Console.ReadLine(), out n))
             {
-                int i = 1; int triangle = 0;
+                int i = 1, triangle = 0;
                 while (i <= n)
                 {
                     triangle += i;
                     i++;
                 }
-                Console.WriteLine("The nth triangular number of {0} is {1}", n, triangle);
+                Console.WriteLine($"The nth triangular number of {n} is {triangle}.");
             }
-            else
-            {
-                Console.WriteLine("Invalid input");
-            }
+            else Console.WriteLine("Invalid input");
+
             Console.WriteLine("\n\nKey 1 to continue with this function, 2 to return to the main menu, or any other key to close the program.");
             switch (Console.ReadKey().Key)
             {
@@ -192,16 +177,11 @@ namespace Mathematics
             if (Int32.TryParse(Console.ReadLine(), out n))
             {
                 int factorial = 1;
-                for (int i = 1; i <= n; i++)
-                {
-                    factorial *= i;
-                }
-                Console.WriteLine("The factorial of {0} is {1}", n, factorial); ;
+                for (int i = 1; i <= n; i++) factorial *= i;
+                Console.WriteLine($"The factorial of {n} is {factorial}.");
             }
-            else
-            {
-                Console.WriteLine("Invalid input");
-            }
+            else Console.WriteLine("Invalid input");
+
             Console.WriteLine("\n\nKey 1 to continue with this function, 2 to return to the main menu, or any other key to close the program.");
             switch (Console.ReadKey().Key)
             {
@@ -226,18 +206,16 @@ namespace Mathematics
             Console.Write("Enter a value for n to find the corresponding nth hexagonal number: ");
             if (Int32.TryParse(Console.ReadLine(), out n))
             {
-                int i = 1; int hexagon = 0;
+                int i = 1, hexagon = 0;
                 while (i <= (n*2)-1)
                 {
                     hexagon += i;
                     i++;
                 }
-                Console.WriteLine("The nth hexagonal number of {0} is {1}", n, hexagon);
+                Console.WriteLine($"The nth hexagonal number of {n} is {hexagon}.");
             }
-            else
-            {
-                Console.WriteLine("Invalid input");
-            }
+            else Console.WriteLine("Invalid input");
+            
             Console.WriteLine("\n\nKey 1 to continue with this function, 2 to return to the main menu, or any other key to close the program.");
             switch (Console.ReadKey().Key)
             {
