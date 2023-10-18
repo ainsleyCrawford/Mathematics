fb39d4b6f591684f27ac616a52414a51a95cdc80
Ainsley Crawford <ainsley.c@hotmail.co.uk>
2023-10-18 08:08:05 +0100

    Incorporated Math.Abs(n) into SquareLoop() Method

--- Square_Numbers.cs
+++ Square_Numbers.cs
@@ -7,18 +7,18 @@ namespace Mathematics
         internal static void SquareLoop()
         {
             int n;
-            Console.Write("Enter a value for n to find the corresponding nth square number: ");
+            Console.Write("Enter a value to calculate its corresponding square number: ");
             if (Int32.TryParse(Console.ReadLine(), out n))
             {
                 int i = 1, diff = 1, square = 0;
-                while (i <= n)
+                while (i <= Math.Abs(n))
                 {
                     square += diff;
                     diff += 2;
                     i++;
                 }
                 Console.ForegroundColor = ConsoleColor.Green;
-                Console.WriteLine($"The nth square number of {n} is {square}.");
+                Console.WriteLine($"The square number of {n} is {square}.");
             }
             else Error.InvalidInput();
 
